ec4d341263d26e4b52bffc5b7521ea171a9b672c
actionbastrd <64579144+actionbastrd@users.noreply.github.com>
2021-06-22 00:21:19 -0500

    Dictionary add
    Now uses dictionary to build set to support pyramidding sets

--- Strength Training Calculator.py
+++ Strength Training Calculator.py
@@ -5,15 +5,41 @@ to achieve one rep max. Uses 4 X 12 as a base"""
 
 # initialize: this should probably be its own function
 # def Start()
-workout = "squat" #input("Workout: ").lower()  #ideally a drop down menu selection(would be cool to include autocomplete)
+def workout():
+    """Returns a dictionary with integers as keys. Value is a list with element 0 = reps, element 1 = weight.
+    Restrictions: Higher weight should always precede lighter weight"""
+    wk = {}
+    xtra = .75  # later used to append extra set, I feel this should be modified to be less arbitrary
+    start_reps = 5
+    sets = 4
+    i = 0
+    for _set in range(sets):
+        wk[i] = [start_reps, current_weight]
+        if wk[i][0] < rep_limit:
+            wk[i][0] += 3
+        elif (i + 1) == sets and wk[i][0] > rep_limit:
+            wk[i][0] = rep_limit
+            sets = sets + 1
+            if len(wk) > set_limit:
+                wk[i][1] = current_weight + add_weight
+                sets = start_sets
+        i += 1
+    return wk
+
+
+
+
+exercise = "squat" #input("Workout: ").lower()  #ideally a drop down menu selection(would be cool to include autocomplete)
 current_max = 275 #int(input("Current max: "))
 target_max = 600 #int(input("Target max: "))
 target_volume = target_max * 30
 start_weight = int(current_max * .75)  # weight/rep
 total_reps_current_strength = target_volume / start_weight
 days_aweek = 3
-limit = 12
-
+start_sets = 4
+rep_limit = 12
+add_weight = 5
+set_limit = 8
 difficulty = 'medium'
 
 if difficulty == 'medium':
@@ -24,9 +50,7 @@ elif difficulty == 'hard':
 print(total_reps_current_strength)
 
 
-xtra = .75   # later used to append extra set, I feel this should be modified to be less arbitrary
-start_reps = 5
-sets = [start_reps] * 4
+
 
 week = 0
 current_weight = start_weight
@@ -37,32 +61,7 @@ while flag:
     volume_week = 0
     for day in range(days_aweek):
         print(day + 1) # day + 1 used for readability and avoid errors due to count offset
-        i = 0
-        if week > 0:
-            if start_reps < limit:
-                start_reps += 2 * modifier     #  might use a for loop here to pyramid each set down as a % of first set in series
-                sets = [start_reps] * 4
-            elif len(sets) < 7:
-                sets.append(int(start_reps * xtra))
-                if len(sets) > 6:
-                    start_reps = 5 #  reset
-                    current_weight += 5 * modifier
-                    sets = [start_reps] * 4
-
-
-        for _set in sets:
-            i += 1 # could avoid use of counter if using len(sets) and notation sets[_set](+ 1 to start count from 1)
-            day_volume = 0
-
-
-
-    #        elif _set < limit:  #  if a set is less than the limit add 3 more according to week
-   #             start_reps += 3 * week
-     #           sets = [start_reps] * 4
-
-
-
-            print("Set:", i,  workout, "weight", current_weight,  "reps", _set)
+        wkout = workout()
             print("Sets check", sets)
             volume_week += _set * current_weight
 
